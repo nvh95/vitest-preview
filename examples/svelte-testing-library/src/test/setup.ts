b70adf24a4611cff08ede3f6528a3cc9517e4409
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-10-25 11:18:27 +0700

    🙌 example: add svelte-testing-library example

---
+++ examples/svelte-testing-library/src/test/setup.ts
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom';
+
+import '../app.css';
+
+// Simulate the structure of `index.html`
+const div = document.createElement('div');
+div.id = 'app';
+document.body.appendChild(div);
