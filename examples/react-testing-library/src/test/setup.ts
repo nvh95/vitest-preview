8b9c0371114e4bce36d7d12aac5e15d8964a03b3
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2025-09-21 17:57:48 +0700

    chore: update example to develop `configure`

--- examples/react-testing-library/src/test/setup.ts
+++ examples/react-testing-library/src/test/setup.ts
@@ -1,6 +1,9 @@
 import '@testing-library/jest-dom';
-
-
+import { configure } from 'vitest-preview';
 
 // Import global css to use with vitest-preview
 import '../index.css';
+
+configure({
+  externalCss: ['src/background.css'],
+});
